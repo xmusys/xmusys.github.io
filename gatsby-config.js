08fc77b51ad820ac49fbc1a887e88cab8e250f09
Qing Ran <163833263+Ran-ddd@users.noreply.github.com>
2024-05-09 12:34:40 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Lab`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
